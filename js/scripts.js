cc4a04603f5b5aea0517ada7fe5cd6c6adfd16fa
Cory West-Forbes and Matt Miller <matt@>
2018-03-26 15:49:44 -0700

    add function to js file to keep track of words in string

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,25 @@ $(function(){
 
     console.log(strToArray(inputString));
 
+    function wordCounter(array) {
+      var counter = 0;
+      var compareWord = array.shift();
+    }
+
+    function wordCounter(array) {
+      var counter = 0;
+      var compareWord = array.shift();
+      console.log(compareWord);
+      array.forEach(function(word) {
+        var words = array.filter(function(word) {
+          return word === compareWord;
+          counter = words.length;
+          console.log(counter);
+        });
+      })
+      console.log(words);
+    }
+
     event.preventDefault();
   });
 });
